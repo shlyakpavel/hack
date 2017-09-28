348a263f008272d199273197f3d752536788cabf
shlyakpavel <shlyak-pavel2011@ya.ru>
2017-09-28 21:43:10 +0000

    First step

---
+++ process.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -X POST \
+    -H "$(cat api.key)" \
+    -H "Content-Type: application/json" \
+    -d '
+    {
+       "inputs": [
+        {
+           "data": {
+               "image": {
+                  "url": "https://www.purinamills.com/2.purinamills.com/media/Images/MasterBrand/Lifestages/horse-special-needs.png"
+                }
+            }
+        }
+        ]
+    }'\
+	https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs
